72aa58a0dee3b33c869f9e2936eaee591106a20b
Heeseong Yu <179582@cloud.rangitoto.school.nz>
2025-08-25 09:43:39 +1200

    Changed the input to numbers rather than full names.

--- Python/games_compendium.py
+++ Python/games_compendium.py
@@ -1,24 +1,29 @@
 """11DGT Python games compendium assessment. Contains text based games which includes speed typing, wordle and paper scissors rock. 
 Each game has a leaderboard system that can be seen."""
 action = 0
-print("Welcome to Games Conpendium!")
 name = input("What is your name? ")
-while action != "exit":
-    action = input("What do you wish to do? (speed typing, wordle, paper scissors rock, leaderboard, exit): ").strip().lower()
-    if action == "speed typing":
-        print("play speed typing")
-        continue
-    elif action == "wordle":
-        print("play wordle")
-        continue
-    elif action == "paper scissors rock":
-        print("play paper scissors rock")
-        continue
-    elif action == "leaderboard":
-        print("show leaderboard")
-        continue
-    elif action == "exit":
-        print("Thank you for playing!")
-        break
-    else:
+print(f"Welcome to Games Conpendium, {name}!")
+while action != "exit": #Keep taking inputs and allows the user to take the following action. The user can keep playing as much as they want.
+    try:
+        action = int(input("What do you wish to do? \n 1 : speed typing \n 2 : wordle \n 3 : paper scissors rock \n 4 : leaderboard, \n 5 : exit \n"))
+    
+        if action == 1:
+            print("play speed typing")
+            continue
+        elif action == 2:
+            print("play wordle")
+            continue
+        elif action == 3:
+            print("play paper scissors rock")
+            continue
+        elif action == 4:
+            print("show leaderboard")
+            continue
+        elif action == 5:
+            print("Thank you for playing!")
+            break
+        else:
+            raise ValueError
+    except ValueError:
         print("Please try again.")
+        continue
