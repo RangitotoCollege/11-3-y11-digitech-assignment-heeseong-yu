194cd632632c1cb648fe77eb598347858d40ffe7
Heeseong <179582@cloud.rangitoto.school.nz>
2025-10-13 09:57:47 +1300

    Added comments to main menu.

--- Python/main_menu.py
+++ Python/main_menu.py
@@ -1,5 +1,6 @@
 """11DGT Python games compendium assessment. Contains text-based games which include speed typing, Wordle and paper, scissors, rock. 
 Each game has a leaderboard system that can be seen. This is the main menu."""
+#Imports function from different Python files.
 import speed_typing
 import wordle
 import paper_scissors_rock
@@ -7,11 +8,14 @@ import utils
 def main_menu():
     action = 0 
     print("=== Name setting ===\n")
-    while len(utils.name) == 0 or len(utils.name) > 20:   #Until the user inputs a name between 1 to 20 letters it keeps asking for the name.
+    #To prevent bugs, until the user inputs a name between 1 to 20 letters it repeats asking for the name.
+    while len(utils.name) == 0 or len(utils.name) > 20:
         print("Name should be between 1 to 20 letters.")
+        #Saves the name in utils file where it can be used across all files.
         utils.set_name(input("What is your name? "))
     print(f"Welcome to Games Conpendium, {utils.name}!")
-    while action != 5: #Keep taking inputs and allow the user to take the following action. The user can keep playing as much as they want.
+    #Until the user wants to exit, they can keep playing the game.
+    while action != 5: 
         try:
             action = int(input("\n === Main Menu === \n What do you wish to do? \n 1 : speed typing \n 2 : wordle \n 3 : paper scissors rock \n 4 : leaderboard, \n 5 : exit \n"))
             if action == 1:
@@ -22,13 +26,16 @@ def main_menu():
                 paper_scissors_rock.play()
             elif action == 4:
                 print("=== Leaderboard ===")
-                for i in range(len(utils.files)): #Loops over all files for all games for each leaderboard viewing.
+                #Loops over all games and corresponding files and prints the corresponding top 3 leaderboard.
+                for i in range(len(utils.games)):
                     utils.view_leaderboard(utils.files[i],i)
             elif action == 5:
                 print("Thank you for playing!")
+        #If action is not any of the options, it asks the user to try again.    
             else:
                 raise ValueError
-        except ValueError: #If action is not any of the options, it asks the user to try again.
+        except ValueError: 
             print("Please try again.")
-if __name__ == "__main__": #Allows the game to only run when it is executed on this file directly.
+#Allows the game to only run when it is executed on this file directly.
+if __name__ == "__main__": 
     main_menu()
