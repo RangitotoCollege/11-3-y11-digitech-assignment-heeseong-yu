c8b391ed21c6b98f7107e1836dc06d6180255b64
Heeseong <179582@cloud.rangitoto.school.nz>
2025-10-14 11:03:46 +1300

    PEP 8 Style Check.

--- Python/main_menu.py
+++ Python/main_menu.py
@@ -1,23 +1,34 @@
-"""11DGT Python games compendium assessment. Contains text-based games which include speed typing, Wordle and paper, scissors, rock. 
-Each game has a leaderboard system that can be seen. This is the main menu."""
-#Imports function from different Python files.
+"""11DGT Python games compendium assessment.
+
+Contains text-based games which are:
+Speed Typing, Wordle and Paper, Scissors, Rock.
+Each game has a leaderboard system that can be seen. This is the main menu.
+"""
+# Imports function from different Python files.
 import speed_typing
 import wordle
 import paper_scissors_rock
 import utils
-def main_menu():
-    action = 0 
+# Allows the game to only run when it is executed on this file directly.
+if __name__ == "__main__":
+    action = 0
     print("=== Name setting ===\n")
-    #To prevent bugs, until the user inputs a name between 1 to 20 letters it repeats asking for the name.
+    # To prevent bugs, only 1 to 20 characters is accpeted as a name.
     while len(utils.name) == 0 or len(utils.name) > 20:
         print("Name should be between 1 to 20 letters.")
-        #Saves the name in utils file where it can be used across all files.
+        # Saves the name in utils file where it can be used across all files.
         utils.set_name(input("What is your name? "))
     print(f"Welcome to Games Conpendium, {utils.name}!")
-    #Until the user wants to exit, they can keep playing the game.
-    while action != 5: 
+    # Until the user wants to exit, they can keep playing the game.
+    while action != 5:
         try:
-            action = int(input("\n === Main Menu === \n What do you wish to do? \n 1 : speed typing \n 2 : wordle \n 3 : paper scissors rock \n 4 : leaderboard, \n 5 : exit \n"))
+            action = int(input("\n=== Main Menu ==="
+                               "\nWhat do you wish to do?,"
+                               "\n1 : speed typing"
+                               "\n2 : wordle"
+                               "\n3 : paper scissors rock"
+                               "\n4 : leaderboard,"
+                               "\n5 : exit\n"))
             if action == 1:
                 speed_typing.play()
             elif action == 2:
@@ -26,16 +37,14 @@ def main_menu():
                 paper_scissors_rock.play()
             elif action == 4:
                 print("=== Leaderboard ===")
-                #Loops over all games and corresponding files and prints the corresponding top 3 leaderboard.
+                # Loops over all games.
+                # Then prints the corresponding top 3 leaderboard for each.
                 for i in range(len(utils.games)):
-                    utils.view_leaderboard(utils.files[i],i)
+                    utils.view_leaderboard(utils.files[i], i)
             elif action == 5:
                 print("Thank you for playing!")
-        #If action is not any of the options, it asks the user to try again.    
+        # If action is not any of the options, it asks the user to try again.
             else:
                 raise ValueError
-        except ValueError: 
+        except ValueError:
             print("Please try again.")
-#Allows the game to only run when it is executed on this file directly.
-if __name__ == "__main__": 
-    main_menu()
--- Python/utils.py
+++ Python/utils.py
@@ -1,64 +1,108 @@
-"""All the functions and variables that are used globally in the file is defined here"""
-name = ""
-games = ["Speed Typing","Wordle","Paper Scissors Rock"]
-files = ["Files\speed_typing_scores.txt","Files\wordle_scores.txt","Files\paper_scissors_rock_scores.txt"]
-def set_name(new_name): #Allows the name to be used in all files by using utils.name and it can be set in the main menu.
+"""File storing useful tools used in all of the files.
+
+ All the functions and variables that are used globally are defined here.
+ """
+name = "name"
+games = ["Speed Typing",
+         "Wordle",
+         "Paper Scissors Rock"]
+files = ["Files\speed_typing_scores.txt",
+         "Files\wordle_scores.txt",
+         "Files\paper_scissors_rock_scores.txt"]
+
+
+def set_name(new_name):
+    """Set the name of the user globally in this file."""
     global name
     name = new_name.strip()
-def replay(extra_message): #Asks the user for replaying the game until the user writes 1 or 0.
+
+
+def replay(extra_message):
+    """Ask the user if they want to replay the same game.
+
+    Extra message can be added for each game.
+    Loops until user writes 1 (yes) or 0 (no).
+    """
     playing = None
-    while playing not in [0,1]:
+    while playing not in [0, 1]:
         try:
-            playing = int(input(f"\nDo you want to play again? {extra_message} \n 0 : No \n 1 : Yes \n"))
-            if playing not in [0,1]:
+            playing = int(input(f"\nDo you want to play again? {extra_message}"
+                                "\n0 : No"
+                                "\n1 : Yes\n"))
+            if playing not in [0, 1]:
                 raise ValueError
         except ValueError:
             print("Please try again.")
     return (playing)
-def overwrite(file,score,index): #Opens a specific file, gets the last line, modifiy it to user's score and rewrite the file with the modification.
+
+
+def overwrite(file, score, index):
+    """Modify the file with a score in the specific index of the file."""
     with open(file, 'r') as f:
         leaderboard = f.readlines()
     leaderboard[index] = score
-    with open(file,'w') as f:
+    with open(file, 'w') as f:
         f.writelines(leaderboard)
+
+
 def find_user(leaderboard):
-    try:
-        for scores in leaderboard:
-            if scores.split()[0] == name:
-                return(leaderboard.index(scores))
-        return(None)
-    except:
-        return(None)
+    """Return the index of the user in the score file."""
+    for scores in leaderboard:
+        if scores.split()[0] == name:
+            return(leaderboard.index(scores))
+    return(None)
+
+
 def check_high_score(file):
-    with open (file,'r') as f:
+    """Return the highest score of the user."""
+    with open (file, 'r') as f:
         leaderboard = f.readlines()
     user_index = find_user(leaderboard)
-    if user_index == None:
+    if user_index is None:
         return (0)
     score = float(leaderboard[user_index].split()[1])
     return(score)
-def add_leaderboard(file,score): #Overwrites the user's score into their personal best. 
-    name_score = str(name) + " " + str(score) + "\n" #Score take the format "name score\n"
-    with open (file,'r') as f:
-        leaderboard = f.readlines()
-    user_index = find_user(leaderboard)
-    if user_index == None: #If the user has no personal score in the file yet, it needs to be newly appended.
-        with open(file,'a') as f: #Appends the score into the last line of the file.
-            f.write(name_score)
-            return()
-    overwrite(file,name_score,user_index)
-            
-def filter_top3(file,reverse_order): #Filters top 3 that is shown on the leaderboard.
+
+
+def add_leaderboard(file, score):
+    r"""Overwrite the user's new high score into their previous high score.
+
+    Score takes the format "name score\n"
+    If the user has no high score yet, it's newly appended to the last line.
+    """
+    name_score = str(name) + " " + str(score) + "\n"
     with open(file, 'r') as f:
         leaderboard = f.readlines()
-    leaderboard = [lines.strip().split() for lines in leaderboard] #Turns the file into a list of lists which contain [name,score]
-    top3 = sorted(leaderboard,key=lambda x: x[1],reverse=reverse_order)[:3]  #Sorts the list based on the score, reverse if speed typing.
+    user_index = find_user(leaderboard)
+    if user_index is None:
+        with open(file, 'a') as f:
+            f.write(name_score)
+            return()
+    overwrite(file, name_score, user_index)
+
+
+def filter_top3(file, reverse_order):
+    """Returns the top 3 scores in the file.
+
+    Turns the file into a list of tuples which contain (name,score)
+    Sorts the list based on the score, reverse if speed typing.
+    """
+    with open(file, 'r') as f:
+        leaderboard = f.readlines()
+    leaderboard = [tuple(lines.strip().split()) for lines in leaderboard]
+    top3 = sorted(leaderboard, key=lambda x: x[1], reverse=reverse_order)[:3]
     return (top3)
-def view_leaderboard(file,game): #Goes over the top 3 for a specific game and prints them in a nice format.
+
+
+def view_leaderboard(file,game):
+    """Go over the top 3 for a specific game and print in a nice format.
+
+    The order is reversed for speed typing as the faster the better.
+    """
     if game == 0:
-        top3 = filter_top3(file,False)
+        top3 = filter_top3(file, False)
     else:
-        top3 = filter_top3(file,True)
+        top3 = filter_top3(file, True)
     print(f"--- {games[game]} ---")
     for score in top3:
         print(" : ".join(score))
