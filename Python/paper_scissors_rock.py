60c10579706aeeb59446b9d0b149015aa7ffc82d
Heeseong <179582@cloud.rangitoto.school.nz>
2025-10-14 15:04:33 +1300

    More PEP8 Style Check.

--- Python/paper_scissors_rock.py
+++ Python/paper_scissors_rock.py
@@ -1,25 +1,32 @@
-"""A game of Paper Scissors Rock"""
+"""A game of Paper Scissors Rock."""
 import random
 import time
 import utils
+
+
 def play():
-    #Similar overall structure to other games.
+    # Similar overall structure to other games.
     streak = 0
     playing = 1
-    #A set of rules is created to determine who wins.
+    # A set of rules is created to determine who wins.
     rules = {
-  ('scissors', 'paper'): 'scissors',
-  ('paper', 'rock'): 'paper',
-  ('rock', 'scissors'): 'rock',
-}
-    #The list keeps track of what hand the user has played until now. 
+        ('scissors', 'paper'): 'scissors',
+        ('paper', 'rock'): 'paper',
+        ('rock', 'scissors'): 'rock',
+    }
+    # The list keeps track of what hand the user has played until now.
     user_hands_frequency = ["paper", "scissors", "rock"]
-    print(f"\n === Paper Scissors Rock === \n Welcome to paper scissors rock {utils.name}! \n Simple game where scissors win paper, paper wins rock and rock wins scissors.\n Same hand is a tie. \n You can keep winning against a computer to increase your win streak, and the highest streak goes in the leaderboard. \n Good luck!")
-    while playing: 
+    print(
+        f"\n === Paper Scissors Rock === \n Welcome to paper scissors rock {utils.name}! \n Simple game where scissors win paper, paper wins rock and rock wins scissors.\n Same hand is a tie. \n You can keep winning against a computer to increase your win streak, and the highest streak goes in the leaderboard. \n Good luck!")
+    while playing:
         print("\n======")
-        if random.randint(1,10) == 10:  #Once in a while, it randomly resets the user frequency to prevent the user from overloading with one hand and then spamming another hand.
+        # Once in a while, it randomly resets the user frequency to prevent the
+        # user from overloading with one hand and then spamming another hand.
+        if random.randint(1, 10) == 10:
             user_hands_frequency = ["paper", "scissors", "rock"]
-        computer_hands_frequency = []  #From what user did, it creates a list for the computer to choose from by inverting the list with hands that win it.
+        # From what user did, it creates a list for the computer to choose from
+        # by inverting the list with hands that win it.
+        computer_hands_frequency = []
         for hands in user_hands_frequency:
             if hands == "paper":
                 computer_hands_frequency.append("scissors")
@@ -27,34 +34,50 @@ def play():
                 computer_hands_frequency.append("rock")
             elif hands == "rock":
                 computer_hands_frequency.append("paper")
-        computer_hand = computer_hands_frequency[random.randint(0,len(computer_hands_frequency)-1)]
+        computer_hand = computer_hands_frequency[random.randint(
+            0, len(computer_hands_frequency) - 1)]
         user_hand = None
-        while user_hand not in user_hands_frequency: #Until the user gives a valid input, it repeats asking what hand they want to play.
+        # Until the user gives a valid input, it repeats asking what hand they
+        # want to play.
+        while user_hand not in user_hands_frequency:
             try:
-                user_hand = input("What hand? \n 0 : Paper \n 1 : Scissors \n 2 : Rock \n")
-                if int(user_hand) > 2:  #User hand above 2 is created by the user, so avoid confusion, it raises a value error.
+                user_hand = input(
+                    "What hand? \n 0 : Paper \n 1 : Scissors \n 2 : Rock \n")
+                # User hand above 2 is created by the user, so avoid confusion,
+                # it raises a value error.
+                if int(user_hand) > 2:
                     raise ValueError
                 user_hand = user_hands_frequency[int(user_hand)]
             except ValueError:
-                user_hand = user_hand.lower().strip() 
-                if user_hand == "paper" or user_hand == "scissors" or user_hand == "rock":  #If the user wrote the hand itself, it still accepts it
+                user_hand = user_hand.lower().strip()
+                # If the user wrote the hand itself, it still accepts it
+                if user_hand == "paper" or user_hand == "scissors" or user_hand == "rock":
                     break
                 print("Invalid input.")
-        user_hands_frequency.append(user_hand)  #The hand user did is appended to the list for the computer to use the winning hand more frequently, as explained above.
-        winner = rules.get((computer_hand, user_hand), rules.get((user_hand, computer_hand), 'tie')) #From the rules, it determines who wins or if it's a tie.
-        print(f"{utils.name}: {user_hand}   Computer: {computer_hand}" )
-        if winner == user_hand: #Winning and losing message and streak, and highest streak system.
+        # The hand user did is appended to the list for the computer to use the
+        # winning hand more frequently, as explained above.
+        user_hands_frequency.append(user_hand)
+        # From the rules, it determines who wins or if it's a tie.
+        winner = rules.get(
+            (computer_hand, user_hand), rules.get(
+                (user_hand, computer_hand), 'tie'))
+        print(f"{utils.name}: {user_hand}   Computer: {computer_hand}")
+        # Winning and losing message and streak, and highest streak system.
+        if winner == user_hand:
             print(f"{utils.name} won!")
             streak += 1
             print(f"You have won {streak} games in a row!")
-            if streak > utils.check_high_score("Files\paper_scissors_rock_scores.txt"): 
-                print(f"New high record! {utils.name} : {streak} games won in a row!")
-                utils.add_leaderboard("Files\paper_scissors_rock_scores.txt",streak)
+            if streak > utils.check_high_score(
+                    "Files\\paper_scissors_rock_scores.txt"):
+                print(
+                    f"New high record! {utils.name} : {streak} games won in a row!")
+                utils.add_leaderboard(
+                    "Files\\paper_scissors_rock_scores.txt", streak)
         elif winner == computer_hand:
             print(f"The computer has won...")
             streak = 0
         else:
             print("It's a tie. You still have your winning streak.")
         playing = utils.replay("STREAK ENDS IF YOU LEAVE! ")
-        time.sleep(0.5) #Just a little delay to prvent user from spamming.
+        time.sleep(0.5)  # Just a little delay to prvent user from spamming.
     return
--- Python/wordle.py
+++ Python/wordle.py
@@ -1,40 +1,58 @@
-"""A game of Wordle"""
+"""A game of Wordle."""
 import random
 import utils
+
+
 def play():
     streak = 0
     playing = 1
-    #Similar overall structure as speed_typing
-    print(f"\n === Wordle === \n Welcome to Wordle {utils.name}! \n Guess a random 5-letter English word in 6 tries. \n If your try contains: \n Correct letter at the correct place, GREEN \n Correct letter but at the wrong place, YELLOW \n Incorrect letter, RED, will be printed accordingly. \n Keep playing to get the highest win streak and go in the leaderboard. \n Type 'quit' or 'exit' to quit the game. \n Good luck!")  
-    terminator = ["quit","exit"]
-    while playing: 
-        tries = 6  #The user has 6 guesses of words.
-        with open("Files\wordle_words.txt","r")  as f:
+    # Similar overall structure as speed_typing
+    print(f"\n === Wordle === \n Welcome to Wordle {utils.name}! \n Guess a random 5-letter English word in 6 tries. \n If your try contains: \n Correct letter at the correct place, GREEN \n Correct letter but at the wrong place, YELLOW \n Incorrect letter, RED, will be printed accordingly. \n Keep playing to get the highest win streak and go in the leaderboard. \n Type 'quit' or 'exit' to quit the game. \n Good luck!")
+    terminator = ["quit", "exit"]
+    while playing:
+        tries = 6  # The user has 6 guesses of words.
+        with open("Files\\wordle_words.txt", "r") as f:
             wordle_words_list = [word.strip() for word in f.readlines()]
-        with open("Files\wordle_answers.txt","r")  as f:
-            wordle_answers_list = [word.strip() for word in f.readlines()]  #There is a seperate list for possible answers and possible guesses to make answers easy while making guesses have more freedom.
-        target_word = wordle_answers_list[random.randint(0,len(wordle_answers_list)-1)]  #Gets a random word from the 5-letter word list
-        while tries != 0:  #Until the user uses all guesses, it repeats asking the user to input a guess.
-            match = []  
+        with open("Files\\wordle_answers.txt", "r") as f:
+            # There is a seperate list for possible answers and possible
+            # guesses to make answers easy while making guesses have more
+            # freedom.
+            wordle_answers_list = [word.strip() for word in f.readlines()]
+        # Gets a random word from the 5-letter word list
+        target_word = wordle_answers_list[random.randint(
+            0, len(wordle_answers_list) - 1)]
+        # Until the user uses all guesses, it repeats asking the user to input
+        # a guess.
+        while tries != 0:
+            match = []
             guess = input("\nGuess : ").lower().strip()
             if guess in terminator:
                 break
             if guess not in wordle_words_list:
                 print("Invalid word.")
-                continue  #If the guess is in the possible solutions, it allows the guess.           
+                # If the guess is in the possible solutions, it allows the
+                # guess.
+                continue
             tries -= 1
-            for i in range(5):  #Goes over the word to check if it is in the right place, in the wrong place, or not at all and appends the corresponding colour in order.
+            # Goes over the word to check if it is in the right place, in the
+            # wrong place, or not at all and appends the corresponding colour
+            # in order.
+            for i in range(5):
                 if guess[i] == target_word[i]:
                     match.append("GREEN")
                 elif guess[i] in target_word:
                     match.append("YELLOW")
                 else:
                     match.append("RED")
-            for colours in match:  #Prints out to the user what letter is in which state and how many tries they have left.
-                print(colours,end = " ")
+            # Prints out to the user what letter is in which state and how many
+            # tries they have left.
+            for colours in match:
+                print(colours, end=" ")
             print(f"\n{tries} tries left.")
-            if guess != target_word:  #If the user correctly guesses the word, it increases their streak and ends the game.
-                if tries == 0: #If the game ends without the user guessing it correctly, the streak resets and the word is revealed.
+            # If the user correctly guesses the word, it increases their streak
+            # and ends the game.
+            if guess != target_word:
+                if tries == 0:  # If the game ends without the user guessing it correctly, the streak resets and the word is revealed.
                     print(f"Unfortunate, the answer was: {target_word}.")
                     streak = 0
                     break
@@ -42,9 +60,10 @@ def play():
             print("Correct!")
             streak += 1
             print(f"You have got {streak} correct answers in a row!")
-            if streak > utils.check_high_score("Files\wordle_scores.txt"): 
-                print(f"New high record! {utils.name} : {streak} correct answers in a row!")
-                utils.add_leaderboard("Files\wordle_scores.txt",streak)
-                break           
+            if streak > utils.check_high_score("Files\\wordle_scores.txt"):
+                print(
+                    f"New high record! {utils.name} : {streak} correct answers in a row!")
+                utils.add_leaderboard("Files\\wordle_scores.txt", streak)
+                break
         playing = utils.replay("STREAK ENDS IF YOU LEAVE! ")
     return
